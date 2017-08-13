ac4dbb1db20fb141f04a8709e3bc32966a39e2eb
parekh9502 <30980806+parekh9502@users.noreply.github.com>
2017-08-13 13:36:49 -0500

    Add files via upload

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'BuyHereDotCom' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for BuyHereDotCom
+  pod 'Fabric'
+  pod 'Crashlytics'
+  #use_frameworks!
+  #pod "VideoSplashKit"
+pod 'RevealingSplashView'
+pod 'TWMessageBarManager'
+pod 'SCLAlertView'
+pod 'SDWebImage'
+pod 'SimpleAnimation'
+pod 'SwipeView'
+pod 'TAPageControl', '~> 0.2’
+pod 'CircleMenu', '~> 2.0.1'
+pod 'PayPal-iOS-SDK'
+pod 'IQKeyboardManagerSwift'
+end
---
+++ SplashViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SplashViewController.swift
+//  
+//
+//  Created by Pritesh Parekh on 11/29/16.
+//  Copyright © 2016 Pritesh Parekh. All rights reserved.
+//
+/*
+ import VideoSplash
+ 
+ class ViewController: VideoSplashViewController {
+ 
+ override func viewDidLoad() {
+ super.viewDidLoad()
+ let url = NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource("test", ofType: "mp4")!)
+ self.videoFrame = view.frame
+ self.fillMode = .ResizeAspectFill
+ self.alwaysRepeat = true
+ self.sound = true
+ self.startTime = 12.0
+ self.duration = 4.0
+ self.alpha = 0.7
+ self.backgroundColor = UIColor.blackColor()
+ self.contentURL = url
+ self.restartForeground = true
+ }
+ }
+ */
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
